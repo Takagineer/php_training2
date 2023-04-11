e21b868f5e498d26bb4fb0dec100dcc450e0af20
Takagineer <itengineer0415@gmail.com>
2023-04-11 20:59:18 +0900

    udpate/DBへの接続_query使用

--- lesson03/sample02.php
+++ lesson03/sample02.php
@@ -10,15 +10,14 @@
 
 <body>
   <?php $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
-  $db->query('drop table if exists test');
-  $success = $db->query('create table test(id INT)');
-  if ($success) {
-    echo 'テーブルを削除して作成しました';
+  $records = $db->query('select count(*)as cnt from my_items');
+  if ($records) {
+    while ($record = $records->fetch_assoc())
+      echo $record['cnt'] . '<br>';
   } else {
-    echo '失敗';
     echo $db->error;
   }
-  echo 'テーブル作成';
+  // var_dump($records);
   ?>
 </body>
 
--- lesson03/sample03.php
+++ lesson03/sample03.php
@@ -10,16 +10,15 @@
 
 <body>
   <?php $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
-  $db->query('drop table if exists test');
-  $success = $db->query('create table test(id INT)');
-  if ($success) {
-    echo 'テーブルを削除して作成しました';
+  $ret = $db->query('insert into memos(memo)values ("phpからのメモです")');
+  // valueの値等でDBのデータを壊されたりする可能性があるため、基本的にはqueryを使用しない
+  if ($ret) {
+    echo 'データをそうにゅうしました';
   } else {
-    echo '失敗';
     echo $db->error;
   }
-  echo 'テーブル作成';
   ?>
+
 </body>
 
 </html>
